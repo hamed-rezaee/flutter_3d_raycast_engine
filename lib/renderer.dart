c1bd42544d46b581c3339aa9209f39be6390ed31
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2024-06-22 10:30:10 +0200

    refactor skybox

--- lib/renderer.dart
+++ lib/renderer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_3d_raycast_engine/enums.dart';
 import 'package:flutter_3d_raycast_engine/player.dart';
 
 class Renderer extends CustomPainter {
@@ -15,12 +16,32 @@ class Renderer extends CustomPainter {
 
   void _drawSky(Canvas canvas, Size size) => canvas.drawRect(
         Rect.fromLTWH(0, 0, size.width, size.height / 2),
-        Paint()..color = Colors.black,
+        Paint()
+          ..color = MaterialInformation.sky.color
+          ..shader = LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              MaterialInformation.sky.color,
+              MaterialInformation.sky.color.withOpacity(0.1),
+            ],
+          ).createShader(Rect.fromLTWH(0, 0, size.width, size.height / 2)),
       );
 
   void _drawGround(Canvas canvas, Size size) => canvas.drawRect(
         Rect.fromLTWH(0, size.height / 2, size.width, size.height / 2),
-        Paint()..color = Colors.green,
+        Paint()
+          ..color = MaterialInformation.ground.color
+          ..shader = LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              MaterialInformation.ground.color.withOpacity(0.5),
+              MaterialInformation.ground.color,
+            ],
+          ).createShader(
+            Rect.fromLTWH(0, size.height / 2, size.width, size.height / 2),
+          ),
       );
 
   void _drawWalls(Canvas canvas) {
